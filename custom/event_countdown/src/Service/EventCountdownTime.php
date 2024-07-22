57c3fc7d8a103e4fba0ec2ed6790070424ed9104
SamoJanezic <janezic.orion@gmail.com>
2024-07-22 16:07:02 +0200

    unified dates

--- custom/event_countdown/src/Service/EventCountdownTime.php
+++ custom/event_countdown/src/Service/EventCountdownTime.php
@@ -11,7 +11,7 @@ class EventCountdownTime {
         $date_time = new DrupalDateTime($eventStartDate, new \DateTimeZone('UTC'));
         $timestamp = $date_time->getTimestamp();
         $startDate = date("Y-m-d", $timestamp);
-        $currentDate = date("Y/m/d");
+        $currentDate = date("Y-m-d");
         $remainingDays = strtotime($startDate) - strtotime($currentDate);
         return floor((($remainingDays/24)/60)/60);
     }
